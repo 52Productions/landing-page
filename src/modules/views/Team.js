cbac565295056d8ddfc00093bf0e56a4312be617
Erica Stephan <erica@ericas-mbp.lan>
2022-02-15 07:02:00 -0800

    reduce file sizes

--- src/modules/views/Team.js
+++ src/modules/views/Team.js
@@ -7,7 +7,7 @@ import Container from '@material-ui/core/Container';
 import Avatar from '@material-ui/core/Avatar'
 import Typography from '../components/Typography';
 import adam_cheyer from '../../assets/adam_cheyer.png';
-import cap from '../../assets/cap.png';
+import jp from '../../assets/jp_beach.jpg';
 import richard_turner from '../../assets/richard_turner_thumbnail.png';
 import asa_turner from '../../assets/asa_turner.png';
 import charles_park from '../../assets/charles_park.png';
@@ -35,12 +35,14 @@ const styles = (theme) => ({
     width: '260px',
     backgroundColor: theme.palette.secondary.light,
     raised: 'false',
-    border: 'none', 
+    border: 'none',
     boxShadow: 'none',
   },
   avatar: {
     height: '250px',
-    width: '250px', 
+    width: '250px',
+    borderWidth: '2px',
+    borderColor: 'black',
     boxShadow: '2px 2px 2px 2px royalblue',
   },
   title: {
@@ -69,29 +71,29 @@ function Team(props) {
         <Grid container spacing={5}>
           <Grid item xs={12} md={4}>
             <div className={classes.item}>
-            <Card className={classes.circleCard}>
-              <Avatar            
+              <Card className={classes.circleCard}>
+                <Avatar
                   className={classes.avatar}
                   src={richard_turner}
                   alt="Richard Turner">
-              </Avatar>
-            </Card>
-            <Typography variant="h6" className={classes.title}>
-              Richard Turner, CEO
-            </Typography>
-            <Typography variant="h5">
-              {'Seen in over 200 countries by over a billion people, multiple awardwinner and the subject of the 5-Star feature documentary film “DEALT”, Turner is a world-renowned Card Mechanic whose skills cannot be replicated, according to experts and scientists from Harvard and the University of Moscow. Turner’s touch and card skills leave audiences baffled beyond comprehension! Over the past fifty years Richard has used his unique gaming knowledge to design interactive puzzles, gambling board games and now conversational-interface gaming apps.'}
-            </Typography>
+                </Avatar>
+              </Card>
+              <Typography variant="h6" className={classes.title}>
+                Richard Turner, CEO
+              </Typography>
+              <Typography variant="h5">
+                {'Seen in over 200 countries by over a billion people, multiple awardwinner and the subject of the 5-Star feature documentary film “DEALT”, Turner is a world-renowned Card Mechanic whose skills cannot be replicated, according to experts and scientists from Harvard and the University of Moscow. Turner’s touch and card skills leave audiences baffled beyond comprehension! Over the past fifty years Richard has used his unique gaming knowledge to design interactive puzzles, gambling board games and now conversational-interface gaming apps.'}
+              </Typography>
             </div>
           </Grid>
           <Grid item xs={12} md={4}>
             <div className={classes.item}>
-            <Card className={classes.circleCard}>
-              <Avatar
+              <Card className={classes.circleCard}>
+                <Avatar
                   className={classes.avatar}
                   src={adam_cheyer}
                   alt="Adam Cheyer" />
-            </Card>
+              </Card>
               <Typography variant="h6" className={classes.title}>
                 Adam Cheyer, COO
               </Typography>
@@ -102,12 +104,12 @@ function Team(props) {
           </Grid>
           <Grid item xs={12} md={4}>
             <div className={classes.item}>
-            <Card className={classes.circleCard}>
-              <Avatar
+              <Card className={classes.circleCard}>
+                <Avatar
                   className={classes.avatar}
-                  src={cap}
+                  src={jp}
                   alt="Jonathan Pauli" />
-            </Card>
+              </Card>
               <Typography variant="h6" className={classes.title}>
                 Jonathan Pauli, Product Director
               </Typography>
@@ -118,12 +120,12 @@ function Team(props) {
           </Grid>
           <Grid item xs={12} md={4}>
             <div className={classes.item}>
-            <Card className={classes.circleCard}>
-              <Avatar
+              <Card className={classes.circleCard}>
+                <Avatar
                   className={classes.avatar}
                   src={asa_turner}
                   alt="Asa Turner" />
-            </Card>
+              </Card>
               <Typography variant="h6" className={classes.title}>
                 Asa Spades Turner, Engineering Director
               </Typography>
@@ -133,13 +135,13 @@ function Team(props) {
             </div>
           </Grid>
           <Grid item xs={12} md={4}>
-          <div className={classes.item}>
-          <Card className={classes.circleCard}>
-              <Avatar
+            <div className={classes.item}>
+              <Card className={classes.circleCard}>
+                <Avatar
                   className={classes.avatar}
                   src={charles_park}
                   alt="Charles Park" />
-            </Card>
+              </Card>
               <Typography variant="h6" className={classes.title}>
                 Charles Park, Creative Director
               </Typography>
